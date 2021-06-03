4b6868e7dd53f5ab8092efed84c3fd3434cd3b22
Mohammad Adeel <adeel.a.mhd@gmail.com>
2021-06-02 23:59:34 -0400

    Add a method to deck.go that deals cards. Run it in main.go to test that it works as intended.

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
+// import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
+
+	hand, remainingDeck := deal(cards, 5)
+	hand.print()
+	remainingDeck.print()
+
+	// fmt.Println(cards)
 }
